ccb802dd21f01b7a33d5bca6fdbdf0971a7c9a09
Pablo Vieira <pablo@Pablos-M1-Pro.local>
2023-10-20 17:42:21 +1100

    fix: revert next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
-  swcMinify: true,
-  experimental: {
-    // Required:
-    appDir: true,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
